f93ada8f6d59cca62464e48cb0afb64c16fc1073
timothy mitchell <timothy.p.mitchell92@gmail.com>
2017-04-17 16:26:32 -0700

    added random number

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 var totalClicks = 0;
 var clicksLeft = 25;
 var items = [];
+var threeRandNum = [];
+var nums = [];
 
 /* This is the constructor function to make each item an object */
 function Item(name) {
@@ -17,26 +19,55 @@ function Item(name) {
 var allItems = ['bag.jpg','banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'
 ];
 
+/* Puts number of items in an array */
+for (var i = 0; i < allItems.length; i++){
+  nums.push(i);
+}
+
+/* This generates a number between 1 and 25 */
+function getRandomNum() {
+  var min = 0;
+  var max = allItems.length;
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+function genThreeNum(){
+  for(var i = 0; i < 3; i++){
+    var randNum = getRandomNum();
+    if (randNum == threeRandNum[i]){
+      // getRandomNum()
+    }
+    else {
+      threeRandNum.push(randNum);
+  }
+}
+}
+
+/* This generates the objects using the constructor */
 function genItems(){
   for (var i = 0; i < allItems.length; i++){
     var newItem = new Item(allItems[i]);
     items.push(newItem);
-}
+  }
 }
 
+/* This pushes images to html */
 function imagesToHtml(){
   for (var i = 0; i < 3; i++){
+    var randNum = getRandomNum()
+    console.log(randNum)
     var img = document.createElement('IMG');
-    img.src = items[i].filepath;
-
-    var images = document.getElementById('images');
+    img.src = items[randNum].filepath;
     document.body.appendChild(img);
   }
 }
 
+
+
+
 function start(){
-    genItems();
-    imagesToHtml();
+  genItems();
+  imagesToHtml();
 }
 
 start();
